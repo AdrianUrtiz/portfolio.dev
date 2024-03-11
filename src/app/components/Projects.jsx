1558ab6090c07b3188b5aa7c9f70a9507bdbfb6e
AdrianUrtiz <116136643+AdrianUrtiz@users.noreply.github.com>
2024-03-11 12:45:25 -0600

    fix id to projects

--- src/app/components/Projects.jsx
+++ src/app/components/Projects.jsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 
 const PROJECTS = [
   {
+    id: 1,
     title: 'SandarInmuebles',
     description:
       'Colaboración para la creación de una e-commerce para la venta de inmuebles, utilizando tecnologías como React, Firebase, Ant Design, entre otras.',
@@ -11,6 +12,7 @@ const PROJECTS = [
     tags: ['React', 'Firebase', 'Ant Design'],
   },
   {
+    id: 2,
     title: 'BANESVIM Veracruz',
     description:
       'Desarrollo front-end de una página web para el gobierno de Veracruz, para mostrar información sobre los casos de violencia de género en el estado. Diseñando y desarrollando la interfaz de usuario, utilizando tecnologías como Javascript, HTML, CSS, MySQL, PHP, entre otras.',
@@ -20,6 +22,7 @@ const PROJECTS = [
     tags: ['Javascript', 'HTML', 'CSS', 'MySQL', 'PHP'],
   },
   {
+    id: 3,
     title: 'SISOGEM',
     description:
       'Desarrollo de un sistema de gestión para organos de gobierno del estado de Michoacán, contacto directo con el cliente para la definición de requerimientos, participando activamente en el desarrollo de la interfaz de usuario y la lógica de negocio. Utilizando tecnologías como React, Node.js, Express, MongoDB, AWS S3, entre otras.',
@@ -29,6 +32,7 @@ const PROJECTS = [
     tags: ['React', 'Node.js', 'Express', 'MongoDB', 'AWS S3'],
   },
   {
+    id: 4,
     title: 'VIVEL Academia de Emmprendimiento',
     description:
       'Participación en el desarrollo de una plataforma de educación en línea, contacto directo con el cliente para la definición de requerimientos y desarrollo de la interfaz de usuario. Utilizando tecnologías como CodeIgniter, MySQL, HTML, CSS, entre otras.',
@@ -43,7 +47,7 @@ const Projects = () => {
   return (
     <>
       {PROJECTS.map((project) => (
-        <article key={project}>
+        <article key={project.id}>
           <h3>{project.title}</h3>
           <p>{project.description}</p>
           <ul>
