7477fe44c6782427b5445eeb722a9f905600b520
AdrianUrtiz <116136643+AdrianUrtiz@users.noreply.github.com>
2024-04-11 19:54:18 -0600

    Complete info to PROJECTS

--- src/app/components/Projects.jsx
+++ src/app/components/Projects.jsx
@@ -1,70 +1,68 @@
-import Image from 'next/image'
+'use client'
 
-const PROJECTS = [
-  {
-    id: 1,
-    title: 'SandarInmuebles',
-    description:
-      'Colaboración para la creación de una e-commerce para la venta de inmuebles, utilizando tecnologías como React, Firebase, Ant Design, entre otras.',
-    link: 'https://sandar-inmuebles.web.app/',
-    github: '#',
-    image: '/sandar.png',
-    tags: ['React', 'Firebase', 'Ant Design'],
-  },
-  {
-    id: 2,
-    title: 'BANESVIM Veracruz',
-    description:
-      'Desarrollo front-end de una página web para el gobierno de Veracruz, para mostrar información sobre los casos de violencia de género en el estado. Diseñando y desarrollando la interfaz de usuario, utilizando tecnologías como Javascript, HTML, CSS, MySQL, PHP, entre otras.',
-    link: 'https://banesvim.ddns.net/banavim/',
-    github: '#',
-    image: '/banesvim.png',
-    tags: ['Javascript', 'HTML', 'CSS', 'MySQL', 'PHP'],
-  },
-  {
-    id: 3,
-    title: 'SISOGEM',
-    description:
-      'Desarrollo de un sistema de gestión para organos de gobierno del estado de Michoacán, contacto directo con el cliente para la definición de requerimientos, participando activamente en el desarrollo de la interfaz de usuario y la lógica de negocio. Utilizando tecnologías como React, Node.js, Express, MongoDB, AWS S3, entre otras.',
-    link: '#',
-    github: '#',
-    image: '/sisogem.png',
-    tags: ['React', 'Node.js', 'Express', 'MongoDB', 'AWS S3'],
-  },
-  {
-    id: 4,
-    title: 'VIVEL Academia de Emmprendimiento',
-    description:
-      'Participación en el desarrollo de una plataforma de educación en línea, contacto directo con el cliente para la definición de requerimientos y desarrollo de la interfaz de usuario. Utilizando tecnologías como CodeIgniter, MySQL, HTML, CSS, entre otras.',
-    link: '#',
-    github: '#',
-    image: '/vivel.png',
-    tags: ['CodeIgniter', 'MySQL', 'HTML', 'CSS'],
-  },
-]
+import Image from 'next/image'
+import GitHub from '../icons/GitHub'
+import Link from '../icons/Link'
+import LinkButton from './LinkButton'
+import PROJECTS from '../const/Projects'
 
 const Projects = () => {
   return (
     <>
       {PROJECTS.map((project) => (
-        <article key={project.id}>
-          <h3 className='text-lg font-semibold mt-4 mb-1 text-green-300 dark:text-green-700'>
-            {project.title}
-          </h3>
-          <p className='text-base mb-4 text-pretty'>{project.description}</p>
-          <ul>
-            {project.tags.map((tag) => (
-              <li key={tag}>{tag}</li>
-            ))}
-          </ul>
-          <Image
-            width={400}
-            height={300}
-            quality={70}
-            className='rounded-lg shadow-xl shadow-slate-200/10'
-            src={project.image}
-            alt={`Captura de pantalla de ${project.title}`}
-          />
+        <article
+          key={project.id}
+          className='flex flex-col space-x-0 space-y-8 group md:flex-row md:space-x-8 md:space-y-0'>
+          <div className='w-full md:w-1/2'>
+            <div className='relative flex flex-col items-center col-span-6 row-span-5 gap-8 transition duration-500 ease-in-out transform shadow-xl overflow-clip rounded-xl sm:rounded-xl md:group-hover:-translate-y-1 md:group-hover:shadow-2xl lg:border lg:border-gray-800 lg:hover:border-gray-700 lg:hover:bg-gray-800/50'>
+              <Image
+                width={400}
+                height={224}
+                quality={70}
+                layout='responsive'
+                className='object-cover object-top w-full h-56 transition duration-500 sm:h-full md:scale-110 md:group-hover:scale-105'
+                src={project.image}
+                alt={`Captura de pantalla de ${project.title}`}
+              />
+            </div>
+          </div>
+
+          <div className='w-full md:w-1/2 md:max-w-lg'>
+            <h3 className='text-2xl font-bold text-gray-100 dark:text-gray-800'>
+              {project.title}
+            </h3>
+            <div class='flex flex-wrap mt-2'>
+              <ul className='flex flex-row mb-2 gap-x-2'>
+                {project.tags.map((tag) => (
+                  <li key={tag.name}>
+                    <span
+                      className={`flex gap-x-2 rounded-full text-xs ${tag.className} py-1 px-2 `}>
+                      <tag.icon className='size-4' />
+                      {tag.name}
+                    </span>
+                  </li>
+                ))}
+              </ul>
+
+              <div className='mt-2 text-gray-400 dark:text-gray-700'>
+                {project.description}
+              </div>
+              <footer className='flex items-end justify-start mt-4 gap-x-4'>
+                {project.github && (
+                  <LinkButton href={project.github}>
+                    <GitHub className='size-6' />
+                    Code
+                  </LinkButton>
+                )}
+                {project.link && (
+                  <LinkButton href={project.link}>
+                    <Link className='size-4' />
+                    Preview
+                  </LinkButton>
+                )}
+              </footer>
+            </div>
+          </div>
         </article>
       ))}
     </>
