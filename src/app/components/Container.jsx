036778742444e82aa9d0e2ad166cf3ddd09c02f2
AdrianUrtiz <116136643+AdrianUrtiz@users.noreply.github.com>
2024-04-11 19:53:40 -0600

    Change to max width of 850px

--- src/app/components/Container.jsx
+++ src/app/components/Container.jsx
@@ -1,6 +1,6 @@
 const Container = ({ children, className, id }) => {
   return (
-    <section id={id} className={`w-full lg:w-[740px] mx-auto ${className}`}>
+    <section id={id} className={`w-full lg:w-[850px] mx-auto ${className}`}>
       {children}
     </section>
   )
