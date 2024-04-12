23196fc8cbe3c820b9767d0e1330a25a3e933871
AdrianUrtiz <116136643+AdrianUrtiz@users.noreply.github.com>
2024-04-11 19:53:10 -0600

    Export const in another file

---
+++ src/app/const/Projects.js
@@ -0,0 +1,42 @@
+import TAGS from '../const/Tags'
+
+const PROJECTS = [
+  {
+    id: 1,
+    title: 'SandarInmuebles',
+    description:
+      'Colaboración para la creación de una e-commerce para la venta de inmuebles, utilizando tecnologías como React, Firebase, Ant Design, entre otras.',
+    link: 'https://sandarinmuebles.com/',
+    image: '/sandar.png',
+    tags: [TAGS.React, TAGS.Firebase],
+  },
+  {
+    id: 2,
+    title: 'BANESVIM Veracruz',
+    description:
+      'Desarrollo front-end de una página web para el gobierno de Veracruz, para mostrar información sobre los casos de violencia de género en el estado. Diseñando y desarrollando la interfaz de usuario, utilizando tecnologías como Javascript, HTML, CSS, MySQL, PHP, entre otras.',
+    link: 'https://banesvim.ddns.net/banavim/',
+    image: '/banesvim.png',
+    tags: [TAGS.Javascript, TAGS.HTML, TAGS.CSS, TAGS.MySQL, TAGS.PHP],
+  },
+  {
+    id: 3,
+    title: 'SISOGEM',
+    description:
+      'Desarrollo de un sistema de gestión para organos de gobierno del estado de Michoacán, contacto directo con el cliente para la definición de requerimientos, participando activamente en el desarrollo de la interfaz de usuario y la lógica de negocio. Utilizando tecnologías como React, Node.js, Express, MongoDB, AWS S3, entre otras.',
+
+    image: '/sisogem.png',
+    tags: [TAGS.React, TAGS.Nodejs, TAGS.Express, TAGS.MongoDB, TAGS.AWS],
+  },
+  {
+    id: 4,
+    title: 'VIVEL Academia de Emprendimiento',
+    description:
+      'Participación en el desarrollo de una plataforma de educación en línea, contacto directo con el cliente para la definición de requerimientos y desarrollo de la interfaz de usuario. Utilizando tecnologías como CodeIgniter, MySQL, HTML, CSS, entre otras.',
+
+    image: '/vivel.png',
+    tags: [TAGS.PHP, TAGS.MySQL, TAGS.HTML, TAGS.CSS],
+  },
+]
+
+export default PROJECTS
---
+++ src/app/const/Tags.js
@@ -0,0 +1,83 @@
+import ReactJS from '../icons/ReactJS'
+import Firebase from '../icons/Firebase'
+import Javascript from '../icons/Javascript'
+import CSS from '../icons/CSS'
+import HTML from '../icons/HTML'
+import MySQL from '../icons/Mysql'
+import PHP from '../icons/Php'
+import Nodejs from '../icons/NodeJS'
+import Express from '../icons/Express'
+import MongoDB from '../icons/Mongo'
+import AWS from '../icons/Aws'
+import Next from '../icons/Next'
+import Tailwind from '../icons/Tailwind'
+
+const TAGS = {
+  React: {
+    name: 'React',
+    className: 'bg-[#003159] text-white',
+    icon: ReactJS,
+  },
+  Firebase: {
+    name: 'Firebase',
+    className: 'bg-firebase text-white',
+    icon: Firebase,
+  },
+  Javascript: {
+    name: 'Javascript',
+    className: 'bg-javascript text-white',
+    icon: Javascript,
+  },
+  HTML: {
+    name: 'HTML',
+    className: 'bg-html text-white',
+    icon: HTML,
+  },
+  CSS: {
+    name: 'CSS',
+    className: 'bg-css text-white',
+    icon: CSS,
+  },
+  MySQL: {
+    name: 'MySQL',
+    className: 'bg-mysql text-white',
+    icon: MySQL,
+  },
+  PHP: {
+    name: 'PHP',
+    className: 'bg-php text-white',
+    icon: PHP,
+  },
+  Nodejs: {
+    name: 'Node.js',
+    className: 'bg-nodejs text-white',
+    icon: Nodejs,
+  },
+  Express: {
+    name: 'Express',
+    className: 'bg-express text-white',
+    icon: Express,
+  },
+  MongoDB: {
+    name: 'MongoDB',
+    className: 'bg-mongodb text-white',
+    icon: MongoDB,
+  },
+  AWS: {
+    name: 'AWS',
+    className: 'bg-aws text-white',
+    icon: AWS,
+  },
+  Next: {
+    name: 'Next.js',
+    className: 'bg-next text-white',
+    icon: Next,
+  },
+  Tailwind: {
+    name: 'Tailwind CSS',
+    className: 'bg-tailwind text-white',
+    icon: Tailwind,
+  },
+}
+
+export default TAGS
